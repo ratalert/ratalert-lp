89a323d0cbc6e641ec015a04cc0af3018c04e962
juggernod <juggernod@protonmail.com>
2022-06-08 12:53:56 +0200

    Fix the newRewardPeriod CLI command

--- bin/cli.js
+++ bin/cli.js
@@ -16,16 +16,16 @@ const commands = {
         const status = await (await this.getInst(contract)).paused();
         console.log(`${contract} is ${status ? 'paused' : 'not paused'}`);
     },
-    newRewardPeriod: async(contract, amount, start, end) => {
+    newRewardPeriod: async(contract,  amount, start, end) => {
         const pool = await artifacts.require(contract).deployed();
-        const foodToken = await artifacts.require('FastFood').deployed();
+        const ticker = contract.substring(0, contract.length - 4);
+        const foodToken = await artifacts.require(ticker).deployed();
         const startDate = new Date(Date.parse(start));
         const endDate = new Date(Date.parse(end));
         const startEpoch = Math.floor(startDate.getTime() / 1000);
         const endEpoch = Math.floor(endDate.getTime() / 1000);
         const currentBlock = (await web3.eth.getBlock('latest')).timestamp;
-        console.log(`Creating new reward period from ${startEpoch} (${startDate.toISOString()}) to ${endEpoch} (${endDate.toISOString()}) with a reward of ${toWei(amount)} (${amount}) FFOOD. Current block: ${currentBlock}.`);
-        // return console.log(startEpoch, endEpoch);
+        console.log(`Creating new reward period from ${startEpoch} (${startDate.toISOString()}) to ${endEpoch} (${endDate.toISOString()}) with a reward of ${toWei(amount)} (${amount}) ${ticker}. Current block: ${currentBlock}.`);
         await this.executeOrEncode(foodToken, 'approve', [pool.address, toWei(amount)]);
         const res = await this.executeOrEncode(pool, 'newRewardPeriod', [toWei(amount), startEpoch, endEpoch]);
         if (res) console.log(res);
